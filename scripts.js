04d034067887406395ebf269b6e91abc3a7a9784
alito0118 <alejotenaglia@gmail.com>
2024-05-29 18:35:59 -0300

    Diseño basico, arreglar scripts

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const llamarFrase = document.getElementById("button");
+
+function ashe(){
+    let ivi = document.getElementById("button").innerHTML = "Que rico se te ve el totito en la wawa";
+    return ivi;
+} 
+
+llamarFrase.addEventListener(onclick, ashe)
