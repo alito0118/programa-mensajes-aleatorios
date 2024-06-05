225ae6261c5470d1c48158b6cd3c20228a5cf48d
alito0118 <alejotenaglia@gmail.com>
2024-06-05 09:41:29 -0300

    Nuevas frases y funcionalidad correcta

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 const llamarFrase = document.getElementById("button");
 
-function ashe(){
-    let ivi = document.getElementById("button").innerHTML = "Que rico se te ve el totito en la wawa";
-    return ivi;
-} 
 
-llamarFrase.addEventListener(onclick, ashe)
+function fraseRandom (){ 
+        arrFrase = ["La nueva era", "Era paleozoica", "Era mesozoica", "Es hora de crear una nueva era","Era popular", "Era industrial", "Era medieval", "Era antartica", "Era tecnologica", "Era espacial"];
+        let i = Math.floor(Math.random()*10);
+        return arrFrase[i];
+};
+
+function ashe() {
+    document.getElementById("frase").innerHTML = `La frase del dia es: ${fraseRandom()}`;
+};
+
+llamarFrase.addEventListener("click", ashe);
